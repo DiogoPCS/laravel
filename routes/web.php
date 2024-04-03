37934f4d8da1ee2de318dc116b499b74eb8371eb
DiogoPCS <diogo.paulino.santos@hotmail.com>
2024-04-03 17:18:51 -0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\PrincipalController::class, 'principal']);
+
+Route::get('/contato', [App\Http\Controllers\ContatoController::class, 'contato']);
+
+Route::get('/sobre-nos', [App\Http\Controllers\SobreNosController::class, 'sobreNos']);
