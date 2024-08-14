e23455945f61bddf469121ddacefb87f15abe8e9
DiogoPCS <diogo.paulino.santos@hotmail.com>
2024-08-14 11:12:11 -0300

    ajuste no nome da tabela de contatos

--- database/migrations/2024_05_15_152336_create_site_contatos_table.php
+++ database/migrations/2024_05_15_152336_create_site_contatos_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('site_contatos', function (Blueprint $table) {
+        Schema::create('contatos', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('nome', 50);
