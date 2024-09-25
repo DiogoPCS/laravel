b9699834225d527a1fcbe2806892b615489fe55b
DiogoPCS <diogo.paulino.santos@hotmail.com>
2024-09-25 13:30:54 -0300

    pequeno ajuste de layotu

--- resources/views/site/layouts/_partials/menu.blade.php
+++ resources/views/site/layouts/_partials/menu.blade.php
@@ -9,26 +9,26 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item text-center pt-2">
-            <a class="nav-link active" href="{{ route('site.principal') }}">
-                <img src="https://cdn1.iconfinder.com/data/icons/real-estate-building-flat-vol-1/104/building__house__home__real__estate-256.png" width="30" height="30">
+            <a class="nav-link" href="{{ route('site.principal') }}">
+                <img src="https://cdn4.iconfinder.com/data/icons/business-pack-7/64/chart-flowchart-organization-planning-distribution-allocation-512.png" width="30" height="30">
                 Início
             </a>
           </li>
           <li class="nav-item text-center pt-2">
             <a class="nav-link" href="{{ route('site.sobrenos') }}">
-                <img src="https://cdn0.iconfinder.com/data/icons/ui-essence/32/_97ui-256.png" width="30" height="30">
+                <img src="https://cdn4.iconfinder.com/data/icons/business-pack-7/64/staff-allocation-human-resource-labor-512.png" width="30" height="30">
                 Sobre Nós
             </a>
           </li>
           <li class="nav-item text-center pt-2">
             <a class="nav-link" href="{{ route('site.contato') }}">
-                <img src="https://cdn1.iconfinder.com/data/icons/modern-universal/32/icon-03-256.png" width="30" height="30">
+                <img src="https://cdn4.iconfinder.com/data/icons/business-pack-7/64/person-employee-management-cycle-organization-512.png" width="30" height="30">
                 Contato
             </a>
           </li>
           <li class="nav-item text-center pt-2">
             <a class="nav-link" href="{{ route('app.clientes') }}">
-                <img src="https://cdn0.iconfinder.com/data/icons/flat-design-business-set-3/24/people-customer-man-256.png" width="30" height="30">
+                <img src="https://cdn3.iconfinder.com/data/icons/human-pack/64/015-group-people-team-organization-employee-community-company-512.png" width="30" height="30">
                 Clientes
             </a>
           </li>
@@ -40,13 +40,13 @@
           </li>
           <li class="nav-item text-center pt-2">
             <a class="nav-link" href="{{ route('app.fornecedores') }}">
-                <img src="https://cdn2.iconfinder.com/data/icons/industry-flat-4/340/quality_check_business_mark_industry_product_box-256.png" width="30" height="30">
+                <img src="https://cdn4.iconfinder.com/data/icons/business-pack-7/64/team-building-effort-intention-advantage-teamwork-512.png" width="30" height="30">
                 Fornecedores
             </a>
           </li>
           <li class="nav-item text-center pt-2">
             <a class="nav-link" href="{{ route('site.login') }}">
-                <img src="https://cdn4.iconfinder.com/data/icons/VistaICO_Toolbar-Icons/256/Login.png" width="30" height="30">
+                <img src="https://cdn3.iconfinder.com/data/icons/future-pack/64/047-information-engineering-automation-recycle-agile-cycle-data-512.png" width="30" height="30">
                 Login
             </a>
           </li>
