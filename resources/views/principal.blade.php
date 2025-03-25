b2d4375296fc1b3062ceb523de5f658e43b4a515
DiogoPCS <diogo.paulino.santos@hotmail.com>
2025-03-25 13:20:49 -0300

    teste

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -1,61 +1,45 @@
-<!doctype html>
-<html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-  <body>
-    
-    
 
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">Início</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/registrar">Registrar-se</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-              </li>
-            </ul>
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+@extends('_partials.body')
+
+@section('conteudo')
+    <div class="container mt-5">
+      <div class="jumbotron text-center" style="background-color: #7F7F7F; color: #E5E5E5; padding: 20px;">
+        <h1 class="display-4">Encontre seu novo amigo!</h1>
+        <p class="lead">Adote um animal e dê a ele um lar cheio de amor.</p>
+        <a class="btn btn-primary btn-lg" href="/registrar" role="button">Registrar-se para Adotar</a>
+      </div>
+
+      <div class="row mt-5">
+        <div class="col-md-4">
+          <div class="card">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQO11M405Scs_BMVqId6DsP7yYwPk9EOZqwQA&s" class="card-img-top" alt="Cachorro">
+            <div class="card-body">
+              <h5 class="card-title">Cachorro</h5>
+              <p class="card-text">Adote um cachorro e ganhe um companheiro fiel.</p>
+              <a href="#" class="btn btn-primary">Adotar</a>
+            </div>
           </div>
         </div>
-      </nav>
-
-
-
-
-
-
-
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
-</html>
+        <div class="col-md-4">
+          <div class="card">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu32GbP6NdP-hrN5dbJgf2PowmZsVEH4LrOQ&s" class="card-img-top" alt="Gato">
+            <div class="card-body">
+              <h5 class="card-title">Gato</h5>
+              <p class="card-text">Adote um gato e tenha um amigo independente e carinhoso.</p>
+              <a href="#" class="btn btn-primary">Adotar</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ5_Q5J1VNwNfWY3uwG59a1FiCCzuOooQnZA&s" class="card-img-top" alt="Outros">
+            <div class="card-body">
+              <h5 class="card-title">Outros</h5>
+              <p class="card-text">Temos outros animais esperando por um lar.</p>
+              <a href="#" class="btn btn-primary">Adotar</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+@endsection
--- resources/views/registrar.blade.php
+++ resources/views/registrar.blade.php
@@ -1,13 +1,39 @@
-<!doctype html>
-<html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Registre-se</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1>Tela registrar</h1>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
-</html>
+@extends('_partials.body')
+
+@section('conteudo')
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card" style="background-color: #fff; border: 1px solid #7F7F7F;">
+                <div class="card-header" style="background-color: #404040; color: #E5E5E5;">
+                    <h3 class="card-title text-center">Cadastro de Usuário</h3>
+                </div>
+                <div class="card-body">
+                    <form  method="POST">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Nome Completo</label>
+                            <input type="text" class="form-control" id="name" name="name" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">E-mail</label>
+                            <input type="email" class="form-control" id="email" name="email" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Senha</label>
+                            <input type="password" class="form-control" id="password" name="password" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="password_confirmation" class="form-label">Confirmar Senha</label>
+                            <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
+                        </div>
+                        <div class="d-grid">
+                            <button type="submit" class="btn btn-primary" style="background-color: #FF1D00; border-color: #FF1D00;">Cadastrar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
