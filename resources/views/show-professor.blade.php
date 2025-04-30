044f08d2073982a636e2e5197fc9e0706f9ea4af
DiogoPCS <diogo.paulino.santos@hotmail.com>
2025-04-30 16:04:18 -0300

    teste

--- resources/views/show-professor.blade.php
+++ resources/views/show-professor.blade.php
@@ -1,7 +1,11 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
 @foreach ($professores as $professor)
-    <h1>{{ $professor->nome }}</h1>
+    <h1>Nome: {{ $professor->nome }}</h1>
+    <h1>Telefone: {{ $professor->telefone }}</h1>
+    <h1>E-mail: {{ $professor->email }}</h1>
+    <hr>
 @endforeach
 
-{{ $professores->links() }}
+
+{{ $professores->links('pagination::bootstrap-4') }}
