8e09fffcf7645578611cc72601eb744e33bcb760
DiogoPCS <diogo.paulino.santos@hotmail.com>
2025-08-06 13:39:20 -0300

    teste

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/teste', function(){
-    return ['nome' => 'Carlitos teves'];
-});
+Route::apiResource('/proprietario', 'App\Http\Controllers\ProprietarioController');
