7fe122e4765a4d9d40f958649219a130374b5a1a
DiogoPCS <diogo.paulino.santos@hotmail.com>
2025-08-20 13:32:22 -0300

    teste

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('teste', function() {});
-Route::get('usuario', function() {});
-Route::get('cliente', function() {});
