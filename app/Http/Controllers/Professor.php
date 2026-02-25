c3ae119c6545945d6e34e457862b7c3b8b55830a
“jjulixms” <jjuliamacielsouza@gmail.com>
2026-02-25 18:24:04 -0300

    kkjjj

---
+++ app/Http/Controllers/Professor.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Professor extends Controller
+{
+    function chamada(){
+        echo 'Escolha sua turma, para fazer chamada:';
+    }
+    function login(){
+        echo 'Faça seu login';
+    }
+    function notas(){
+        echo 'Escolha sua turma, para colocar as notas:';
+    }
+  
+    
+}
+
+
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,18 @@ use App\Http\Middleware\LogAcessoMiddleware;
 */
 
 Route::get('/', [App\Http\Controllers\Principal::class, 'principal']);
+Route::get('/contato', [App\Http\Controllers\Contato::class, 'contato']);
+Route::get('/boletim', [App\Http\Controllers\Aluno::class, 'boletim']);
+Route::get('/rematricula', [App\Http\Controllers\Aluno::class, 'rematricula']);
+Route::get('/email_institucional', [App\Http\Controllers\Aluno::class, 'enviarEmail']);
+Route::get('/retirar_documentos', [App\Http\Controllers\Aluno::class, 'retirar_documentos']);
+Route::get('/controle_faltas', [App\Http\Controllers\Aluno::class, 'controle_faltas']);
+Route::get('/login', [App\Http\Controllers\Aluno::class, 'login']);
+Route::get('/login', [App\Http\Controllers\Professor::class, 'login']);
+Route::get('/chamada', [App\Http\Controllers\Professor::class, 'chamada']);
+Route::get('/notas', [App\Http\Controllers\Professor::class, 'notas']);
+
+
 
 
 
