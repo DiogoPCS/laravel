5b1b9be97b90ec9b19596372178a49acaf84d7d5
guihensoares <guilhers53292@gmail.com>
2026-02-25 18:25:31 -0300

    coisasNovas

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,19 @@ use App\Http\Middleware\LogAcessoMiddleware;
 */
 
 Route::get('/', [App\Http\Controllers\Principal::class, 'principal']);
+Route::get('/contato', [App\Http\Controllers\Contato::class, 'contato']);
+
+Route::get('/aluno/login', [App\Http\Controllers\Aluno::class, 'login']);
+Route::get('/aluno/boletim', [App\Http\Controllers\Aluno::class, 'boletim']);
+Route::get('/aluno/horario', [App\Http\Controllers\Aluno::class, 'horario']);
+Route::get('/aluno/consultar', [App\Http\Controllers\Aluno::class, 'consultar']);
+Route::get('/aluno/rematricula', [App\Http\Controllers\Aluno::class, 'rematricula']);
+Route::get('/aluno/pedir_documento', [App\Http\Controllers\Aluno::class, 'documento']);
+Route::get('/aluno/ficha_desempenho', [App\Http\Controllers\Aluno::class, 'ficha']);
+Route::get('/aluno/reconsideracao', [App\Http\Controllers\Aluno::class, 'reconsiderar']);
+Route::get('/aluno/vida_escolar', [App\Http\Controllers\Aluno::class, 'vida']);
+
+Route::get('/professor/chamada', [App\Http\Controllers\Professor::class, 'chamada']);
 
 
 
