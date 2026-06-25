1d1af4c902a65666408d4e8bc192b58d3dc42f6c
PedrinzxCampos <pedrohenriquejacklaercio@gmail.com>
2026-06-25 16:07:10 -0300

    AAA

--- resources/views/curso/index.blade.php
+++ resources/views/curso/index.blade.php
@@ -1,45 +1,41 @@
 <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
 <div class="container">
 
     <h1 class="titulo">Cadastrar Curso</h1>
 
     <form action="{{ route('curso.add') }}" method="post" class="form-box">
         @csrf
+
         <label for="nome">Nome</label>
-        <input type="text" name="nome" id="nome" placeholder="Digite o Curso" value="{{ old('nome') }}">
+        <input type="text" name="nome" id="nome"
+               placeholder="Digite o Curso"
+               value="{{ old('nome') }}">
 
-        <label class="titulo-periodo">Período</label>
-        <div class="grupo-radios">
-        <div class="opcao-radio">
-            <input type="radio" id="manha" name="periodo" value="Manhã" checked>
-            <label for="manha">Manhã</label>
-        </div>
+        <label for="periodo">Período</label>
+        <center><select name="periodo" id="periodo">
+            <option value="">Selecione</option>
+            <option value="Manhã">Manhã</option>
+            <option value="Tarde">Tarde</option>
+            <option value="Noite">Noite</option>
+        </select></center>
 
-        <div class="opcao-radio">
-            <input type="radio" id="tarde" name="periodo" value="Tarde">
-            <label for="tarde">Tarde</label>
-        </div>
-
-        <div class="opcao-radio">
-            <input type="radio" id="noite" name="periodo" value="Noite">
-            <label for="noite">Noite</label>
-        </div>
-        </div>
-    
         <button type="submit">Salvar</button>
     </form>
+
     <br>
+
     @isset($success)
         <h1>{{ $success }}</h1>
     @endisset
 
     @if($errors->any())
-            <ul>
-                @foreach($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        @endif
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
 
     <table>
         <tr>
@@ -49,24 +45,19 @@
         </tr>
 
         @isset($curso)
-            @foreach($curso as $curso)
+            @foreach($curso as $item)
                 <tr>
-                    <td>
-                        <h3>{{ $curso->nome }}</h3>
-                    </td>
+                    <td><h3>{{ $item->nome }}</h3></td>
+                    <td><h3>{{ $item->periodo }}</h3></td>
 
                     <td>
-                        <h3>{{ $curso->periodo }}</h3>
-                    </td>
-
-                    <td>
-                        <form action="{{ route('curso.remove', ['id' => $curso->id]) }}" method="GET">
+                        <form action="{{ route('curso.remove', ['id' => $item->id]) }}" method="GET">
                             <button type="submit">Remover</button>
                         </form>
                     </td>
 
                     <td>
-                        <form action="{{ route('curso.atualizar', ['id' => $curso->id]) }}" method="GET">
+                        <form action="{{ route('curso.atualizar', ['id' => $item->id]) }}" method="GET">
                             <button type="submit">Atualizar</button>
                         </form>
                     </td>
@@ -75,4 +66,4 @@
         @endisset
     </table>
 
-    </div>
+</div>
