7e2578f14cc345f54f3adc41bc3795e93c79819b
Takeshi347 <mateus2908@gmail.com>
2026-07-28 22:12:54 -0300

    28-07-2026

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,16 @@
+<!-- basico seria o layout 'pai' -->
+@extends('layouts.basico') 
+
+<!-- vírgula serve para colocar o valor direto -->
+<!-- endsection serve para colocar vários valores -->
+@section('titulo', 'Minha página principal')
+
+
+
+<!-- Abre o Section -->
+@section('conteudo')
+    <h1>Página Principal</h1>
+
+<!-- Fecha o Section -->
+@endsection
+
