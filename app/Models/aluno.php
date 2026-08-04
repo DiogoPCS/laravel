183f139f737d74eb498795a412f68ed93c81d139
DiogoPCS <diogo.paulino.santos@Hotmail.com>
2026-08-04 14:51:27 -0300

    teste

--- app/Models/aluno.php
+++ app/Models/aluno.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class AlunoModel extends Model
+class aluno extends Model
 {
-    use HasFactory;
     protected $table = 'aluno';
-    protected $fillable = ['id', 'nome'];
+    protected $fillable = ['nome', 'email'];
+    use HasFactory;
 }
--- database/migrations/2026_08_04_174202_create_aluno_table.php
+++ database/migrations/2026_08_04_174202_create_aluno_table.php
@@ -15,9 +15,10 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string('nome');
+            $table->string('email');
         });
     }
-
+    
     /**
      * Reverse the migrations.
      */
